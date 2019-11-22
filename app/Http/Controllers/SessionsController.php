4b51e21a8958ea2e6cb24456044f8e30159cd751
郭彦栋 <15513680486@163.com>
2019-11-22 15:02:29 +0800

    管理员可删除用户

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -9,6 +9,7 @@ class SessionsController extends Controller
 
     public function __construct()
     {
+        //Auth 中间件提供的 guest 选项，用于指定一些只允许未登录用户访问的动作，因此我们需要通过对 guest 属性进行设置，只让未登录用户访问登录页面和注册页面
         $this->middleware('guest', [
             'only' => ['create']
         ]);
@@ -26,14 +27,19 @@ class SessionsController extends Controller
             'email' => 'required|email|max:255',
             'password' => 'required'
         ]);
+        //前面我们介绍过的 Auth::attempt() 方法可接收两个参数，
+        //第一个参数为需要进行用户身份认证的数组，第二个参数为是否为用户开启『记住我』功能的布尔值
         if (Auth::attempt($credentials, $request->has('remember'))) {
             // 登录成功后的相关操作
             session()->flash('success', '欢迎回来！');
+            //我们在 store 方法内使用了 Laravel 提供的 Auth::user() 方法来获取 当前登录用户 的信息，并将数据传送给路由
             $fallback = route('users.show', Auth::user());
             return redirect()->intended($fallback);
         } else {
             // 登录失败后的相关操作
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            //这时如果尝试输入错误密码则会显示登录失败的提示信息。
+            //使用 withInput() 后模板里 old('email') 将能获取到上一次用户提交的内容，这样用户就无需再次输入邮箱等内容
             return redirect()->back()->withInput();
         }
 
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,12 +11,30 @@ class UsersController extends Controller
 
     public function __construct()
     {
+        //Laravel 中间件 (Middleware) 为我们提供了一种非常棒的过滤机制来过滤进入应用的 HTTP 请求，
+        //例如，当我们使用 Auth 中间件来验证用户的身份时，如果用户未通过身份验证，则 Auth 中间件会把用户重定向到登录页面。
+        //如果用户通过了身份验证，则 Auth 中间件会通过此请求并接着往下执行。
+        //Laravel 框架默认为我们内置了一些中间件，例如身份验证、CSRF 保护等。
+        //所有的中间件文件都被放在项目的 app/Http/Middleware 文件夹中。
+        //接下来让我们使用 Laravel 提供身份验证（Auth）中间件来过滤未登录用户的 edit, update 动作。
+
+        //第一个为中间件的名称，第二个为要进行过滤的动作。我们通过 except 方法来设定 指定动作 不使用 Auth 中间件进行过滤，
+        //意为 —— 除了此处指定的动作以外，所有其他动作都必须登录用户才能访问，类似于黑名单的过滤机制。
+        //相反的还有 only 白名单方法，将只过滤指定动作。我们提倡在控制器 Auth 中间件使用中，首选 except 方法，这样的话，当你新增一个控制器方法时，默认是安全的，此为最佳实践。
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store', 'index']
         ]);
+        //Auth 中间件提供的 guest 选项，用于指定一些只允许未登录用户访问的动作，因此我们需要通过对 guest 属性进行设置，只让未登录用户访问登录页面和注册页面
         $this->middleware('guest', [
             'only' => ['create']
         ]);
+    }
+
+    public function index()
+    {
+//        $users = User::all();
+        $users =$users = User::paginate(10);
+        return view('users.index', compact('users'));
     }
 
     public function create()
@@ -42,6 +60,7 @@ class UsersController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
+        //如果要让一个已认证通过的用户实例进行登录
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
@@ -49,12 +68,14 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        //授权策略
         $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(User $user, Request $request)
     {
+        //授权策略
         $this->authorize('update', $user);
         $this->validate($request, [
             'name' => 'required|max:50',
@@ -68,5 +89,13 @@ class UsersController extends Controller
         $user->update($data);
         session()->flash('success', '个人资料更新成功！');
         return redirect()->route('users.show', $user->id);
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
     }
 }
